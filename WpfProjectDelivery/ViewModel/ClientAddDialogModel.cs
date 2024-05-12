7f7d630cd621b354590828d808f3f3925cf3429a
muzykos <77461733+muzykos@users.noreply.github.com>
2024-05-12 18:23:16 +0200

    clientadd

---
+++ WpfProjectDelivery/ViewModel/ClientAddDialogModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WpfProjectDelivery.Model;
+
+namespace WpfProjectDelivery.ViewModel
+{
+    public class ClientAddDialogModel
+    {
+        public ObservableCollection<Client> Clients { get; set; }
+        ICommand AddClient_click { get; set; }
+        ICommand Cancel_click { get; set; }
+
+        public ClientAddDialogModel()
+        {
+            Clients = new ObservableCollection<Client>();
+
+            AddClient_click = new RelayCommand(AddClient);
+            Cancel_click = new RelayCommand(Cancel);
+        }
+
+        private void Cancel(object obj)
+        {
+            Window.GetWindow((FrameworkElement)obj).Close();
+        }
+
+        private void AddClient(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- WpfProjectDelivery/ViewModel/ClientsViewModel.cs
+++ WpfProjectDelivery/ViewModel/ClientsViewModel.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 using WpfProjectDelivery.Model;
 using WpfProjectDelivery.View;
 
@@ -12,12 +15,34 @@ namespace WpfProjectDelivery.ViewModel
     public class ClientsViewModel
     {
         public ObservableCollection<Client> Clients { get; set; }
+        public ICommand AddClient_click { get; }
+        public ICommand RemoveClient_click { get; }
+        public ICommand EditClient_click { get; }
         public ClientsViewModel() { 
             ClientsList clientsList = new ClientsList();
             Clients = clientsList.Clients;
             clientsList.AddClient(new Client("Alex","alex@alex.alex",1234,new Address("A","B","C","D","AA-BBB")));
             clientsList.AddClient(new Client("Barnuch", "Barnuch@Barnuch.Barnuch", 1234,new Address("A","B","C","D","AA-BBB")));
+
+            AddClient_click = new RelayCommand(AddClient);
+            RemoveClient_click = new RelayCommand(RemoveClient);
+            EditClient_click = new RelayCommand(EditClient);
         }
 
+        private void EditClient(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RemoveClient(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddClient(object obj)
+        {
+            Window window = new ClientAddDialog();
+            window.ShowDialog();
+        }
     }
 }
