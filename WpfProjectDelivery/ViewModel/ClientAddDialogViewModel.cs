a4b0762579094bcb0278a86f5b2acf0cab294236
muzykos <77461733+muzykos@users.noreply.github.com>
2024-05-12 19:35:52 +0200

    nazewnictwo

---
+++ WpfProjectDelivery/ViewModel/ClientAddDialogViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WpfProjectDelivery.Model;
+
+namespace WpfProjectDelivery.ViewModel
+{
+    public class ClientAddDialogViewModel
+    {
+        public ICommand AddClient_click { get; set; }
+        public ICommand Cancel_click { get; set; }
+
+        public ClientsList clientsList;
+
+        
+
+
+        public ClientAddDialogViewModel()
+        {
+            clientsList = ClientsList.GetInstance();
+            
+
+            AddClient_click = new RelayCommand(AddClient);
+            Cancel_click = new RelayCommand(Cancel);
+        }
+
+        private void Cancel(object obj)
+        {
+            Window.GetWindow((FrameworkElement)obj).Close();
+        }
+
+        private void AddClient(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- WpfProjectDelivery/ViewModel/ClientEditDialogViewModel.cs
+++ WpfProjectDelivery/ViewModel/ClientEditDialogViewModel.cs
@@ -1,31 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Input;
+using System.Windows;
 using WpfProjectDelivery.Model;
 
 namespace WpfProjectDelivery.ViewModel
 {
-    public class ClientAddDialogModel
+
+    public class ClientEditDialogViewModel
     {
-        public ICommand AddClient_click { get; set; }
+        public ICommand EditClient_click { get; set; }
         public ICommand Cancel_click { get; set; }
 
         public ClientsList clientsList;
 
-        
-
-
-        public ClientAddDialogModel()
+        public ClientEditDialogViewModel()
         {
             clientsList = ClientsList.GetInstance();
-            
 
-            AddClient_click = new RelayCommand(AddClient);
+
+            EditClient_click = new RelayCommand(EditClient);
             Cancel_click = new RelayCommand(Cancel);
         }
 
@@ -34,9 +31,11 @@ namespace WpfProjectDelivery.ViewModel
             Window.GetWindow((FrameworkElement)obj).Close();
         }
 
-        private void AddClient(object obj)
+        private void EditClient(object obj)
         {
             throw new NotImplementedException();
         }
     }
+
+
 }
