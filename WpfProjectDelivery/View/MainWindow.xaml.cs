2a1be50b7fe052a3e2b49b83d4be5cad6dadee47
Riazaki <103073478+Riazaki@users.noreply.github.com>
2024-06-09 12:24:29 +0200

    Tłumaczenie

--- WpfProjectDelivery/View/MainWindow.xaml.cs
+++ WpfProjectDelivery/View/MainWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Globalization;
+using System.Threading;
 
 namespace WpfProjectDelivery.View
 {
@@ -28,9 +30,37 @@ namespace WpfProjectDelivery.View
             InitializeComponent();
         }
 
-  
-       
+        private void LangButton_Clikc(object sender, RoutedEventArgs e)
+        {
+            SetLang(((Button)sender).Tag.ToString());
+        }
 
-        
+        private void SetLang(string lang)
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo(lang);
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(lang);
+
+            Application.Current.Resources.MergedDictionaries.Clear();
+            ResourceDictionary resourceDictionary = new ResourceDictionary()
+            {
+                Source = new Uri($"/Dictionary-{lang}.xaml",UriKind.Relative)
+            };
+            Application.Current.Resources.MergedDictionaries.Add(resourceDictionary);
+
+            LangPL.IsEnabled = true;
+            LangEN.IsEnabled = true;
+
+            switch (lang)
+            {
+                case "en-US":
+                    LangEN.IsEnabled = false;
+                    break;
+                case "pl-PL":
+                    LangPL.IsEnabled = false;
+                    break ;
+                default:
+                    break;
+            }
+        }
     }
 }
